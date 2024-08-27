ba1190aba68a6e761c9f46b4ed14f7411e37558b
Jay Kid <cyph3rfox@gmail.com>
2024-08-27 18:53:26 +0200

    feat: add random delays in backend responses to visualize shifting order of requests from client

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,17 @@ const port = 3000;
 const secret =
   "This is the secret you are trying to retrieve that will unlock the door to Vault 76, if you can retrieve it completely that is :))";
 
-app.get("/secret/:secretCharIndex", (req, res) => {
+app.get("/secret/:secretCharIndex", async (req, res) => {
   const { secretCharIndex } = req.params;
   if (secretCharIndex > 0 && secretCharIndex <= 130) {
-    const randomness = Math.random() > 0.1;
-    return res.send(randomness ? secret[secretCharIndex - 1] : "");
+    const shouldReturnCorrectPayload = Math.random() > 0.1;
+    const shouldDelayResponse = Math.random() > 0.6;
+    if (shouldDelayResponse) {
+      await new Promise((resolve) => setTimeout(resolve, 500));
+    }
+    return res.send(
+      shouldReturnCorrectPayload ? secret[secretCharIndex - 1] : ""
+    );
   }
   return res.send("");
 });
